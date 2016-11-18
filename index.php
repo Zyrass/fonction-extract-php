439455ffa5f3d3c40e576bfc75aa9514a11e48e4
Zyrass <zyrass@outlook.fr>
2016-11-18 12:08:51 +0100

    Ajout des fichiers nécéssaire au bon fonctionnement

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>La fonction Extract en PHP</title>
+        <meta name="description" content="Apprentissage de la fonction extract () en php">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- My CSS -->
+        <link rel="stylesheet" type="text/css" href="css/myStyle.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <header>
+            <h1>LA fonction extract () en PHP</h1>
+            <h2>Description d'origine :</h2>
+            <p>Dans ce tuto Php en vidéo, vous apprendrez à vous servir de la fonction extract () de PHP pour vous simplifier le traitement de données issues des formulaires.</p>
+            <p><a href="http://fr.tuto.com/php/la-fonction-extract-php,8895.html">Cliquez ici pour accédez directement à la vidéo</a></p>
+        </header>
+        <section>
+            
+        </section>
+        
+
+
+        <!-- Script JS -->
+        <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/myScript.js"></script>
+    </body>
+</html>
